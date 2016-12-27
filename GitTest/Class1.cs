b8285e420f7f771009258b1db1ac0aff8889b0a7
Raheel Allauddin <rxallaud@hotmail.com>
2016-12-26 22:25:54 -0500

    A new file added.

--- GitTest/Class1.cs
+++ GitTest/Class1.cs
@@ -6,12 +6,7 @@ using System.Threading.Tasks;
 
 namespace GitTest
 {
-    class Program
+    class Class1
     {
-        static void Main(string[] args)
-        {
-            // Hello world 
-            // today world
-        }
     }
 }
